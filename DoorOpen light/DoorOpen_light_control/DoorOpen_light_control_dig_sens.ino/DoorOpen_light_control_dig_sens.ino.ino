f4e0fe63688ad650addf703c2b0eca8f6267d5ca
bj- <bmw518@bk.ru>
2016-12-20 00:42:04 +0300

    light control

--- DoorOpen light/DoorOpen_light_control/DoorOpen_light_control_dig_sens.ino/DoorOpen_light_control_dig_sens.ino.ino
+++ DoorOpen light/DoorOpen_light_control/DoorOpen_light_control_dig_sens.ino/DoorOpen_light_control_dig_sens.ino.ino
@@ -8,7 +8,7 @@
  */
 
 #include <Wire.h> 
-#include <LiquidCrystal_I2C.h>
+#include <LiquidCrystal_I2C.h> // https://github.com/marcoschwartz/LiquidCrystal_I2C
 
 LiquidCrystal_I2C lcd(0x27,16,2);  // set the LCD address to 0x27 for a 16 chars and 2 line display
 
