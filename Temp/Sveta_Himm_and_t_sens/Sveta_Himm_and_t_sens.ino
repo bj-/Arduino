3537b70961ca7cc985a8b6eaa845e9e7036b7019
bj- <bmw518@bk.ru>
2017-12-18 00:56:10 +0300

    Change for other DS1302 lib

--- Temp/Sveta_Himm_and_t_sens/Sveta_Himm_and_t_sens.ino
+++ Temp/Sveta_Himm_and_t_sens/Sveta_Himm_and_t_sens.ino
@@ -40,6 +40,7 @@
 // include the library code:
 #include <LiquidCrystal.h>
 #include "DHT.h"
+#include <stdio.h>
 #include <DS1302.h>
 
 // initialize the library with the numbers of the interface pins
@@ -59,10 +60,57 @@ LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
 DHT dht(DHTPIN, DHTTYPE);
 
+namespace {
+
+// Set the appropriate digital I/O pin connections. These are the pin
+// assignments for the Arduino as well for as the DS1302 chip. See the DS1302
+// datasheet:
+//
+//   http://datasheets.maximintegrated.com/en/ds/DS1302.pdf
+const int kCePin   = 6;  // Chip Enable
+const int kIoPin   = 7;  // Input/Output
+const int kSclkPin = 8;  // Serial Clock
+
+// Create a DS1302 object.
+DS1302 rtc(kCePin, kIoPin, kSclkPin);
+
+String dayAsString(const Time::Day day) {
+  switch (day) {
+    case Time::kSunday: return "Sunday";
+    case Time::kMonday: return "Monday";
+    case Time::kTuesday: return "Tuesday";
+    case Time::kWednesday: return "Wednesday";
+    case Time::kThursday: return "Thursday";
+    case Time::kFriday: return "Friday";
+    case Time::kSaturday: return "Saturday";
+  }
+  return "(unknown day)";
+}
+/*
+void printTime() {
+  // Get the current time and date from the chip.
+  Time t = rtc.time();
+
+  // Name the day of the week.
+  const String day = dayAsString(t.day);
+
+  // Format the time and date and insert into the temporary buffer.
+  char buf[50];
+  snprintf(buf, sizeof(buf), "%s %04d-%02d-%02d %02d:%02d:%02d",
+           day.c_str(),
+           t.yr, t.mon, t.date,
+           t.hr, t.min, t.sec);
+
+  // Print the formatted string to serial so we can see the time.
+  Serial.println(buf);
+}
+*/
+}  // namespace
+
 // Init the DS1302
-DS1302 rtc(6, 7, 8);
+//DS1302 rtc(6, 7, 8);
 // Init a Time-data structure
-Time t;
+//Time t;
 
 int OnTimeH = 10;
 int OnTimeM = 00;
@@ -81,26 +129,33 @@ void setup() {
   lcd.begin(8, 2);
 
   // Set the clock to run-mode, and disable the write protection
-  rtc.halt(false);
+//  rtc.halt(false);
+//  rtc.writeProtect(false);
   rtc.writeProtect(false);
-
+  rtc.halt(false);
+  
    Serial.begin(9600);
 
    pinMode(RelayPin, OUTPUT);
 
-// The following lines can be commented out to use the values already stored in the DS1302
-
+  // Make a new time object to set the date and time.
+  // Sunday, September 22, 2013 at 01:38:50.
+//  Time t(2017, 12, 17, 21, 39, 40, Time::kSunday);
+//    rtc.time(t);
 /*
+// The following lines can be commented out to use the values already stored in the DS1302
 //  rtc.setDOW(SATURDAY);        // Set Day-of-Week to FRIDAY
-  rtc.setDOW(WEDNESDAY);        // Set Day-of-Week to FRIDAY
-  rtc.setTime(07, 58, 40);     // Set the time to 12:00:00 (24hr format)
-  rtc.setDate(13, 04, 2016);   // Set the date to August 6th, 2010
+//  rtc.setDOW(WEDNESDAY);        // Set Day-of-Week to FRIDAY
+  rtc.setDOW(SUNDAY);        // Set Day-of-Week to FRIDAY
+  rtc.setTime(20, 51, 00);     // Set the time to 12:00:00 (24hr format)
+  rtc.setDate(17, 12, 2017);   // Set the date to August 6th, 2010
 //*/
 }
 
 void loop() {
 
-  t = rtc.getTime();
+  //t = rtc.getTime();
+  Time t = rtc.time();
 
   //lcd.setCursor(0, 0);
   //lcd.print(t.hour, DEC);
@@ -108,13 +163,14 @@ void loop() {
 
   // Clear display
   lcd.setCursor(0, 0);
-  lcd.print("        ");
+  lcd.print(" hh       ");
   lcd.setCursor(0, 1);
   lcd.print("        ");
 
 
   lcd.setCursor(0, 0);
-  lcd.print(t.hour, DEC);
+//  lcd.print(t.hour, DEC);
+  lcd.print(t.hr, DEC);
   lcd.setCursor(2, 0);
   lcd.print(":");
   lcd.setCursor(3, 0);
@@ -127,9 +183,10 @@ void loop() {
 
   // Send Day-of-Week and time
   Serial.print("It is the ");
-  Serial.print(t.dow, DEC);
+//  Serial.print(t.dow, DEC);
   Serial.print(". day of the week ");
-  Serial.print(t.hour, DEC);
+  Serial.print(t.hr, DEC);
+//  Serial.print(t.hour, DEC);
   Serial.print(":");
   Serial.print(t.min, DEC);
   Serial.print(":");
@@ -142,9 +199,11 @@ void loop() {
   lcd.setCursor(3, 1);
   lcd.print("10-22");
 
-  if (t.hour >= OnTimeH & t.min >= OnTimeM)
+//  if (t.hour >= OnTimeH & t.min >= OnTimeM)
+  if (t.hr >= OnTimeH & t.min >= OnTimeM)
   {
-     if (t.hour >= OffTimeH & t.min >= OffTimeM )
+//     if (t.hour >= OffTimeH & t.min >= OffTimeM )
+     if (t.hr >= OffTimeH & t.min >= OffTimeM )
      {
        digitalWrite(RelayPin, LOW);
        RelayStatus = "Off";
@@ -174,9 +233,10 @@ delay(3000);
   // Reading temperature or humidity takes about 250 milliseconds!
   // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
   int h = dht.readHumidity();
-  int t = dht.readTemperature();
+//  int t = dht.readTemperature();
+  int tm = dht.readTemperature();
 
-  if (isnan(t) || isnan(h)) 
+  if (isnan(tm) || isnan(h)) 
   {
     lcd.setCursor(0, 0);
     lcd.print("DHT sens");
@@ -192,7 +252,7 @@ delay(3000);
     lcd.print("   % RH ");
 
     lcd.setCursor(0, 0);
-    lcd.print(t);
+    lcd.print(tm);
     lcd.setCursor(0, 1);
     lcd.print(h);
   }
